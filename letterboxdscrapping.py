6cb9fb8a6387ec213dec3fc513f3eb9fc7daa7ee
SyRoxS <leo.moreaumenanteau@gmail.com>
2023-12-27 10:40:07 +0100

    nice

--- letterboxdscrapping.py
+++ letterboxdscrapping.py
@@ -5,29 +5,21 @@ import re
 def getgenders(urllocal):
     r = requests.get(urllocal)
 
-
-
-
     data = r.text
 
     soup = BeautifulSoup(data, 'html.parser')
 
-
     element_with_data_target_link = soup.find('div', {'data-target-link': True})
-
 
     if element_with_data_target_link:
         data_target_link_content = element_with_data_target_link['data-target-link']
-        print("Content inside data-target-link:", data_target_link_content)
     else:
-        print("Element with data-target-link attribute not found.")
+        print('error')
 
     url = "https://letterboxd.com"+str(data_target_link_content)
-    print("url: " + url)
     r2 = requests.get(url)
 
     data = r2.text
-    print(len(r2.text))
     soup = BeautifulSoup(data, 'html.parser')
     genders = soup.find('script', {'type': 'application/ld+json'})
 
@@ -38,7 +30,7 @@ def getgenders(urllocal):
         genre_content = match.group(1)
         # Split the content into a list of genres
         genres = re.findall(r'"([^"]*)"', genre_content)
-        print("Movie genres:", genres)
+        return genres
     else:
-        print("dommage")
+        return "ERROR"
 
