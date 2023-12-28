4fd776b046e38946c880503a4c716abb189ade00
SyRoxS <leo.moreaumenanteau@gmail.com>
2023-12-28 10:42:35 +0100

    okay okay

--- letterboxdscrapping.py
+++ letterboxdscrapping.py
@@ -2,68 +2,91 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import csv
+class MovieDataProcessor:
+    def __init__(self):
+        self.DateOfInsertInList = []
+        self.ReleaseDates = [] 
+        self.Genres = []
+        self.Runtime = [] 
+        self.Directors = []
+        self.Movies = []
+        self.letterboxdURI = []
+        self.count = 0
+        
 
-def getdata(urllocal):
-    r = requests.get(urllocal)
+    def readCSV(self, path):
+            with open(path, mode="r", encoding="utf8") as OpenFile:
+                FileContent = csv.reader(OpenFile)
+                next(FileContent)  # Supprime la première ligne car pas de films présents dessus
+                
+                for row in FileContent:
+                    if row[0][:4] == '2023': #only get data from movies watched in 2023
+                        self.Movies.append(row[1])
+                        self.ReleaseDates.append(row[2])
+                        self.letterboxdURI.append(row[3])
 
-    data = r.text
+    def getdata(urllocal):
+        r = requests.get(urllocal)
 
-    soup = BeautifulSoup(data, 'html.parser')
+        data = r.text
 
-    element_with_data_target_link = soup.find('div', {'data-target-link': True})
+        soup = BeautifulSoup(data, 'html.parser')
 
-    if element_with_data_target_link:
-        data_target_link_content = element_with_data_target_link['data-target-link']
-    else:
-        print('error')
+        element_with_data_target_link = soup.find('div', {'data-target-link': True})
 
-    url = "https://letterboxd.com"+str(data_target_link_content)
-    r2 = requests.get(url)
+        if element_with_data_target_link:
+            data_target_link_content = element_with_data_target_link['data-target-link']
+        else:
+            print('error')
 
-    data = r2.text
+        url = "https://letterboxd.com"+str(data_target_link_content)
+        r2 = requests.get(url)
 
-    return data
+        data = r2.text
 
-def getgenders(data):
+        return data
+
+    def getgenders(data):
+
+        soup = BeautifulSoup(data, 'html.parser')
+
+        jsondata = soup.find('script', {'type': 'application/ld+json'})
+        jsondata = str(jsondata)
+
+        pattern = re.compile(r'"genre":\s*\[([^\]]*)\]')
+        match = pattern.search(jsondata)
+        if match:
+            genre_content = match.group(1)
+            # Split the content into a list of genres
+            genres = re.findall(r'"([^"]*)"', genre_content)
+            return genres
+        else:
+            return "ERROR"
+
+    def getdirector(data):
+        soup = BeautifulSoup(data, 'html.parser')
+
+        jsondata = soup.find('script', {'type': 'application/ld+json'})
+        jsondata = str(jsondata)
+
+        pattern = re.compile(r'"director":\s*\[([^\]]*)\]')
+        match = pattern.search(jsondata)
+        if match:
+            director_content = match.group(1)
+            # Split the content into a list of genres
+            director = re.findall(r'"([^"]*)"', director_content)
+            director_name = re.findall(r'"name":\s*"([^"]*)"', director_content)
+            return director_name
+        else:
+            return "ERROR"
+
+    def getruntime(data):
+        soup = BeautifulSoup(data, 'html.parser')
+
+        pattern = re.compile(r'runTime:\s*(\d+)')
+        match = pattern.search(data)
+        runtime_value = match.group(1)
+
+        return runtime_value
+
     
-    soup = BeautifulSoup(data, 'html.parser')
-
-    jsondata = soup.find('script', {'type': 'application/ld+json'})
-    jsondata = str(jsondata)
-
-    pattern = re.compile(r'"genre":\s*\[([^\]]*)\]')
-    match = pattern.search(jsondata)
-    if match:
-        genre_content = match.group(1)
-        # Split the content into a list of genres
-        genres = re.findall(r'"([^"]*)"', genre_content)
-        return genres
-    else:
-        return "ERROR"
-
-def getdirector(data):
-    soup = BeautifulSoup(data, 'html.parser')
-
-    jsondata = soup.find('script', {'type': 'application/ld+json'})
-    jsondata = str(jsondata)
-
-    pattern = re.compile(r'"director":\s*\[([^\]]*)\]')
-    match = pattern.search(jsondata)
-    if match:
-        director_content = match.group(1)
-        # Split the content into a list of genres
-        director = re.findall(r'"([^"]*)"', director_content)
-        director_name = re.findall(r'"name":\s*"([^"]*)"', director_content)
-        return director_name
-    else:
-        return "ERROR"
-
-def getruntime(data):
-    soup = BeautifulSoup(data, 'html.parser')
-
-    pattern = re.compile(r'runTime:\s*(\d+)')
-    match = pattern.search(data)
-    runtime_value = match.group(1)
-
-    return runtime_value
-
